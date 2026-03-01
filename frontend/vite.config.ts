5b28d0f61d662159fc8ab176b93d86a4ce558171
Issac Roy <issacroy05@gmail.com>
2026-03-01 11:46:47 -0800

    fix: basename

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/Overfit/",
   server: {
     host: "::",
     port: 8080,
