4d3f3447a301fe78cc7b9410e71f04f491a91e23
Issac Roy <issacroy05@gmail.com>
2026-03-18 17:58:55 -0700

    feat: custom domain

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -16,7 +16,7 @@ const App = () => (
     <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter basename="/GhostWriter">
+        <BrowserRouter basename="/">
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/sign-in" element={<SignIn />} />
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, path.resolve(__dirname, ".."), "");
   return {
     envDir: path.resolve(__dirname, ".."),
-    base: "/GhostWriter/",
+    base: "/",
     server: {
       host: "::",
       port: 8080,
